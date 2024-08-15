0d551377cb974e39c975723d107dd40fe854cd3d
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2024-08-15 18:15:09 +0900

    [Silver III] Title: 고양이 카페, Time: 132 ms, Memory: 15528 KB -BaekjoonHub

---
+++ 백준/Silver/28353. 고양이 카페/고양이 카페.java
@@ -0,0 +1,40 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr =new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = N-1;
+        int count =0;
+        while (start<end){
+            int sum = arr[start]+arr[end];
+
+            if(sum<=target){
+                count++;
+                start++;
+                end--;
+            }else{
+                end--;
+            }
+        }
+        System.out.println(count);
+    }
+}
