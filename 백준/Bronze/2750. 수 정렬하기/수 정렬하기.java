806d24a10c8390b6f1fc53c0aa4275e922b7d1a0
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2024-08-16 15:57:59 +0900

    [Bronze II] Title: 수 정렬하기, Time: 120 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,28 +1,37 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int []array = new int[N];
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
         for (int i = 0; i < N; i++) {
-            array[i]=sc.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 0; i < N-1; i++) {
-            for (int j = 0; j < N-1-i; j++) {
-                if(array[j]>array[j+1]){
-                    int temp = array[j+1];
-                    array[j+1]=array[j];
-                    array[j]=temp;
-                    
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                 }
-                
             }
-            
         }
-        for (int i = 0; i < N; i++) {
-            System.out.println(array[i]);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append('\n');
         }
-        
+        System.out.println(sb);
     }
+
+
 }
