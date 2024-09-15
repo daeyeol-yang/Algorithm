a3cf4c9ac47d5f6848ecd24059b6369dbe8b93ad
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2024-09-16 02:19:38 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 104 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+        System.out.println(n.divide(m));
+        System.out.print(n.remainder(m));
+
+    }
+}
