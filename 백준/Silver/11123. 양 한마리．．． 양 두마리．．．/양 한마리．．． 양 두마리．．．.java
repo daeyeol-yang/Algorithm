8a06de6887f4eaa479fc58863360562c4ac3bb7a
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2024-09-17 16:20:36 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 188 ms, Memory: 20748 KB -BaekjoonHub

---
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
@@ -0,0 +1,62 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int N,M;
+
+    public static char[][] map;
+    public static boolean[][] visited;
+
+    public static int[] dx = { 0,0,1,-1};
+    public static int[] dy = { 1,-1,0,0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int countA = Integer.parseInt(br.readLine());
+
+        for (int count = 0; count < countA; count++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            map= new char[N][M];
+            visited= new boolean[N][M];
+            for (int i = 0; i < N; i++) {
+                String temp = br.readLine();
+                for (int j = 0; j < M; j++) {
+                    map[i][j]= temp.charAt(j);
+                }
+            }
+
+            int sol =0;
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if(!visited[i][j]&&map[i][j]=='#'){
+                        dfs(j,i);
+                        sol++;
+
+                    }
+                }
+            }
+            System.out.println(sol);
+        }
+    }
+
+    public static void dfs(int x, int y){
+        visited[y][x] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int next_x = x+dx[i];
+            int next_y = y+dy[i];
+
+            if(next_x>=0&&next_y>=0&&next_x<M&&next_y<N&&!visited[next_y][next_x]&&map[next_y][next_x]=='#'){
+                dfs(next_x,next_y);
+            }
+        }
+    }
+}
