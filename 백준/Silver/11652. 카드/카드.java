ba545159325ddbf11e5fe9db7034a69a9f6ebd24
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2024-10-11 00:16:22 +0900

    [Silver IV] Title: 카드, Time: 344 ms, Memory: 34384 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,7 +1,13 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,24 +15,27 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         HashMap<Long, Integer> hashMap = new HashMap<>();
 
-        // 숫자를 입력받아 빈도를 저장
         for (int i = 0; i < N; i++) {
             long num = Long.parseLong(br.readLine());
-            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+            hashMap.put(num, hashMap.getOrDefault(num,0)+1);
         }
 
-        // HashMap을 리스트로 변환하여 정렬
-        ArrayList<Map.Entry<Long, Integer>> mapList = new ArrayList<>(hashMap.entrySet());
+        ArrayList<Map.Entry<Long,Integer>> mapList = new ArrayList<>(hashMap.entrySet());
 
-        // Comparator로 값(빈도)과 키(숫자)를 기준으로 정렬
-        Collections.sort(mapList, (o1, o2) -> {
-            if (o1.getValue().equals(o2.getValue())) {
-                return o1.getKey().compareTo(o2.getKey());
+        Collections.sort(mapList, new Comparator<Map.Entry<Long, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Long, Integer> o1, Map.Entry<Long, Integer> o2) {
+                if(o1.getValue().equals(o2.getValue())){
+                    return o1.getKey().compareTo(o2.getKey());
+                }
+                return o2.getValue().compareTo(o1.getValue());
             }
-            return Integer.compare(o2.getValue(), o1.getValue()); // 빈도 비교
         });
 
-        // 가장 빈도가 높은 숫자를 출력
         System.out.println(mapList.get(0).getKey());
+
+
+
+
     }
 }
