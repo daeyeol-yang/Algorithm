5c7e45f52c3d9ccc254097a2ea7fb0debcbd606e
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2024-10-16 13:15:07 +0900

    [Silver II] Title: N번째 큰 수, Time: 748 ms, Memory: 282736 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,19 +13,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int [][] arr = new int[N][N];
-        ArrayList<Integer> arrayList = new ArrayList<>();
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        for (int i = 0; i < N; i++) {
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            for (int j = 0; j < N; j++) {
+//                arr[i][j]= Integer.parseInt(st.nextToken());
+//                arrayList.add(arr[i][j]);
+//            }
+//        }
+//
+//        Collections.sort(arrayList,Collections.reverseOrder());
+//
+//        System.out.println(arrayList.get(N-1));
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                arr[i][j]= Integer.parseInt(st.nextToken());
-                arrayList.add(arr[i][j]);
+               queue.add(Integer.parseInt(st.nextToken()));
             }
+
         }
 
-        Collections.sort(arrayList,Collections.reverseOrder());
+        for (int i = 0; i < N-1; i++) {
+            queue.poll();
+        }
 
-        System.out.println(arrayList.get(N-1));
-
+        System.out.println(queue.poll());
 
     }
 }
