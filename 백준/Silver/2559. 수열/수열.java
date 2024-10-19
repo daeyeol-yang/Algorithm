ef4b11cb697d36e5d0405adb56da74e9e36df1a1
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2024-10-19 15:20:10 +0900

    [Silver III] Title: 수열, Time: 236 ms, Memory: 24100 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -3,7 +3,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import javax.swing.ImageIcon;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,24 +14,33 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        int[] pArr = new int[N+1];
+        int[] arr = new int[N+1];
 
-        st = new StringTokenizer(br.readLine());
+        st= new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+//        for (int i = 1; i <= N; i++) {
+//            arr[i]= Integer.parseInt(st.nextToken());
+//        }
+//        System.out.println(Arrays.toString(arr));
+//
+//        for (int i = 1; i <=N ; i++) {
+//            pArr[i] = pArr[i-1]+arr[i];
+//        }
+//        System.out.println(Arrays.toString(pArr));
+
+        for (int i = 1; i <= N ; i++) {
+            pArr[i] = pArr[i-1] + Integer.parseInt(st.nextToken());
         }
 
         int max = Integer.MIN_VALUE;
 
         for (int i = 0; i < N-K+1; i++) {
-            int tempSum=0;
-            for (int j = 0; j < K; j++) {
-                tempSum += arr[i+j];
-            }
-            max = Math.max(tempSum,max);
+            max = Math.max(max,pArr[i+K]-pArr[i]);
         }
 
         System.out.println(max);
+
+
     }
 }
