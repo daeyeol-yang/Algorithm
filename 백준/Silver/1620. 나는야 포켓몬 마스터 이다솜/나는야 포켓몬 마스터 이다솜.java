1fe18e2f4d9739c8e748e587aad89a0a353833ba
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2024-10-19 15:43:37 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 444 ms, Memory: 55352 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,25 +10,63 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        HashMap<String, String> hashMap = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
-            String s = br.readLine();
+        HashMap<String, Integer> hashMap1 = new HashMap<>();
+        HashMap<Integer, String> hashMap2 = new HashMap<>();
 
-            hashMap.put(String.valueOf(i+1), s);
-            hashMap.put(s, String.valueOf(i+1));
+        for (int i = 1; i <= N; i++) {
+            String temp = br.readLine();
+            hashMap1.put(temp, i);
+            hashMap2.put(i, temp);
 
         }
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             String s = br.readLine();
+            char c = s.charAt(0);
 
-            if (hashMap.containsKey(s)) {
-                sb.append(hashMap.get(s)).append('\n');
+            if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z') {
+                sb.append(hashMap1.get(s)).append('\n');
+            } else {
+                sb.append(hashMap2.get(Integer.parseInt(s))).append('\n');
+
             }
+
         }
         System.out.println(sb);
     }
 }
+
+
+//        String[] arr = new String[N];
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = br.readLine();
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 0; i < M; i++) {
+//            String s = br.readLine();
+//            char c = s.charAt(0);
+//
+//            if('a'<=c && c<='z' || 'A'<=c && c<='Z'){
+//                for (int j = 0; j < arr.length; j++) {
+//                    if(arr[j].equals(s)){
+//                        sb.append(j+1).append('\n');
+//                        break;
+//                    }
+//                }
+//
+//            }else{
+//                sb.append(arr[Integer.parseInt(s)-1]).append('\n');
+//            }
+//        }
+//
+//        System.out.println(sb);
+//}
+//}
