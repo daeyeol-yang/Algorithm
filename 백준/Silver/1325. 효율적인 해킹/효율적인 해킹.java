b68ece2abc20b88d0238ed3be79b5f594de2fd26
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2024-11-04 16:13:36 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10884 ms, Memory: 146700 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,70 +1,78 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    public static ArrayList<Integer>[] arrayLists;
     public static boolean[] visited;
-    public  static ArrayList<Integer>[] map;
-    public static int[] arr;
+    public static int count;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        map = new ArrayList[N];
-        for (int i = 0; i < N; i++) {
-            map[i] = new ArrayList<>();
-        }
-        arr = new int[N];
+        int test = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < M; i++) {
+
+        arrayLists = new ArrayList[N];
+
+        for (int i = 0; i < N; i++) {
+            arrayLists[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < test; i++) {
             st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken())-1;
-            int num_related = Integer.parseInt(st.nextToken())-1;
+            int child = Integer.parseInt(st.nextToken())-1;
+            int parent = Integer.parseInt(st.nextToken())-1;
 
-            map[num].add(num_related);
+            arrayLists[parent].add(child);
         }
 
-        for (int i = 0; i < map.length; i++) {
+        int maxChild =0;
+        int [] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            
             visited = new boolean[N];
-            dfs(i);
+            arr[i] = dfs(i);
+            maxChild = Math.max(maxChild,arr[i]);
         }
-
-        int max =0;
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            max = Math.max(arr[i],max);
-        }
-
-        StringBuilder sb  = new StringBuilder();
-
-        for (int i = 0; i < N; i++) {
-            if(arr[i]==max){
+            if(arr[i]==maxChild){
                 sb.append(i+1).append(" ");
             }
         }
 
-
         System.out.println(sb);
+
+
     }
 
-    public static void dfs(int n){
-        visited[n]=true;
-        for (int i : map[n]){
-            if(!visited[i]){
+//    public static void dfs(int node){
+//        visited[node] = true;
+//
+//        for(int next_node : arrayLists[node]){
+//            if(!visited[next_node]){
+//                dfs(next_node);
+//                count+=1;
+//            }
+//        }
+//
+//    }
+    public static int dfs(int node){
+        visited[node] = true;
+        int ret =1;
 
-                arr[i]++;
-                dfs(i);
-
-
+        for(int next_node : arrayLists[node]){
+            if(!visited[next_node]){
+                ret+= dfs(next_node);
+                
             }
         }
-
+        return ret;
 
     }
 }
