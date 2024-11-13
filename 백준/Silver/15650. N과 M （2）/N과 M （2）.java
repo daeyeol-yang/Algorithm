a6399642c1e682467d2575ff94f3469686a27063
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2024-11-14 00:08:46 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -3,42 +3,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] arr;
 
-    public static boolean[] visited;
+    public static int n,r;
+
+    public static ArrayList<Integer> arrayList;
+
+
+    public static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int r = Integer.parseInt(st.nextToken());
+        arrayList = new ArrayList<>();
 
-        arr = new int[r];
 
-        visited = new boolean[n];
 
-        go(n,r,0,0);
+
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = i+1;
+        }
+
+
+        combi(0);
+
     }
 
-    public static void go(int n,int r,int depth,int start){
-        if(depth==r){
-            for(int num : arr){
-                System.out.print(num+" ");
+    public static void combi(int start){
+        if(r == arrayList.size()){
+            for(int num : arrayList){
+                System.out.print(num +" ");
             }
             System.out.println();
             return;
         }
 
-        for (int i = 1; i <= n ; i++) {
-            if(!visited[i-1]&&i>start){
-                visited[i-1]=true;
-                arr[depth] = i;
-                go(n,r,depth+1,i);
-                visited[i-1]=false;
-            }
+        for (int i = start; i <n; i++) {
+            arrayList.add(arr[i]);
+            combi(i+1);
+            arrayList.remove(Integer.valueOf(arr[i]));
         }
+
     }
 }
