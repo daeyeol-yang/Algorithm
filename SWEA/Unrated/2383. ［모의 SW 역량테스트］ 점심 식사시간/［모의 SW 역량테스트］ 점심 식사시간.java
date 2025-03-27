6b1f86596f593c2806d0da1d8485e14be633045e
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2025-03-27 23:19:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 점심 식사시간, Time: 104 ms, Memory: 29,824 KB -BaekjoonHub

--- SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
+++ SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
@@ -1,259 +1,119 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution {
 
-    static class Person implements Comparable<Person>{
-        int x;
-        int y;
-        int stairTime;
-        int endTime;
+    static class Person {
+        int x, y;
+        int[] arrival = new int[2];
+
         public Person(int x, int y) {
-            super();
             this.x = x;
             this.y = y;
-
         }
-
-        public void setStairTime(int time) {
-            this.stairTime = time;
-        }
-
-
-        @Override
-        public int compareTo(Person o) {
-            // TODO Auto-generated method stub
-            return Integer.compare(this.stairTime, o.stairTime);
-        }
-
     }
 
-    static class Stair{
-        int x;
-        int y;
-        int time;
+    static class Stair {
+        int x, y, time;
 
         public Stair(int x, int y, int time) {
-            super();
             this.x = x;
             this.y = y;
             this.time = time;
         }
     }
 
-    public static List<Person> copyPersonList(List<Person> original) {
-        List<Person> copy = new ArrayList<>();
-        for (Person p : original) {
-            Person newP = new Person(p.x, p.y); // 위치 복사
-            copy.add(newP);
-        }
-        return copy;
-    }
+    static int N, personCount;
+    static List<Person> people;
+    static Stair[] stairs;
+    static int answer;
 
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        // TODO Auto-generated method stub
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int TESTCASE = Integer.parseInt(br.readLine());
-
+        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
-        for(int testCase = 1 ; testCase <=TESTCASE ; testCase++) {
+        for (int testCase = 1; testCase <= T; testCase++) {
+            N = Integer.parseInt(br.readLine());
+            people = new ArrayList<>();
+            stairs = new Stair[2];
+            int stairIdx = 0;
 
-            sb.append("#").append(testCase).append(" ");
-
-            int N = Integer.parseInt(br.readLine());
-
-
-            int[][] map = new int[N][N];
-
-            List<Person> personList = new ArrayList<>();
-
-
-            List<Stair> stairList = new ArrayList<>();
-
-            for(int i = 0; i< N ; i++) {// 사람과 계단 입력값 정하기!!
-                st = new StringTokenizer(br.readLine());
-                for(int j = 0; j< N ; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-
-
-                    if(map[i][j] == 1) {
-                        personList.add(new Person(j,i));
-                    }else if(map[i][j] >1) {
-                        stairList.add(new Stair(j,i,map[i][j]));
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    int val = Integer.parseInt(st.nextToken());
+                    if (val == 1) {
+                        people.add(new Person(j, i));
+                    } else if (val > 1) {
+                        stairs[stairIdx++] = new Stair(j, i, val);
                     }
                 }
             }
 
-            int personSize = personList.size();
-
-
-            int solTime =Integer.MAX_VALUE;
-            for(int bit = 0 ; bit < (1<<personSize) ; bit++) {//비트 마스킹을 통해서 사람들이 각각 몇번 계단으로 갈지 정하는 코드!!
-                List<Person> personCopy = copyPersonList(personList);
-
-
-                List<Integer> stair1 = new ArrayList<>();//1번 계단으로 가는 사람들!!
-                List<Integer> stair2 = new ArrayList<>();//2번 계단으로 가는 사람들!!
-
-
-                for(int i = 0; i< personSize ;i++) {
-                    if((bit&(1<<i))!=0) {
-                        stair1.add(i);
-                    }else {
-                        stair2.add(i);
-                    }
-
+            personCount = people.size();
+            for (Person p : people) {
+                for (int s = 0; s < 2; s++) {
+                    Stair stair = stairs[s];
+                    p.arrival[s] = Math.abs(p.x - stair.x) + Math.abs(p.y - stair.y) + 1;
                 }
-
-
-                //사람마다 계단까지 도착하는 시간!!!
-                Stair stairTemp1 = stairList.get(0);
-                Stair stairTemp2 = stairList.get(1);
-
-
-                PriorityQueue<Person> stair1Queue = new PriorityQueue<>();
-                PriorityQueue<Person> stair2Queue = new PriorityQueue<>();
-                //System.out.println(stair1);
-                //System.out.println(stair2);
-
-                for(int i =0 ; i< stair1.size() ; i++) {
-                    int stairPerson = stair1.get(i);
-
-                    Person person = personCopy.get(stairPerson);
-
-                    int time = getDistance(person.x,person.y ,stairTemp1.x,stairTemp1.y)+1;
-
-                    person.setStairTime(time);
-                    stair1Queue.add(person);
-                }
-
-
-                Set<Person> stair1Set = new HashSet<>();
-
-                int timeSol1 =0;
-
-                while(true) {
-                    timeSol1++;
-
-                    List<Person> toRemove = new ArrayList<>();
-
-                    for (Person p : stair1Set) {
-                        p.endTime++;
-                        if (p.endTime >= stairTemp1.time) {
-                            //System.out.println("1에서 탈출 :" + p);
-
-                            toRemove.add(p);
-                        }
-                    }
-
-                    stair1Set.removeAll(toRemove);
-
-                    if(stair1Queue.isEmpty()&&stair1Set.isEmpty()){
-
-                        break;
-                    }
-
-                    //System.out.println("셋1 사이즈" + stair1Set.size());
-                    //System.out.println("큐1 사이즈" + stair1Queue.size());
-
-                    while(!stair1Queue.isEmpty() && timeSol1>= stair1Queue.peek().stairTime){
-                        //System.out.println("hi");
-                        if (stair1Set.size() < 3) {
-                            stair1Set.add(stair1Queue.poll());
-                        }else{
-                            break;
-                        }
-                    }
-
-
-                }
-
-                //System.out.println(timeSol1);
-
-
-
-                for(int i =0 ; i< stair2.size() ; i++) {
-                    int stairPerson = stair2.get(i);
-
-                    Person person = personCopy.get(stairPerson);
-
-                    int time = getDistance(person.x,person.y ,stairTemp2.x,stairTemp2.y)+1;
-
-                    person.setStairTime(time);
-                    stair2Queue.add(person);
-                }
-
-                Set<Person> stair2Set = new HashSet<>();
-
-                int timeSol2 =0;
-                while(true) {
-                    timeSol2++;
-
-                    List<Person> toRemove = new ArrayList<>();
-
-                    for (Person p : stair2Set) {
-                        p.endTime++;
-                        if (p.endTime >= stairTemp2.time) {
-                            //System.out.println("2에서 탈출" +p);
-                            toRemove.add(p);
-                        }
-                    }
-
-                    stair2Set.removeAll(toRemove);
-
-                   // System.out.println("time"+timeSol2);
-
-
-                    if(stair2Queue.isEmpty()&&stair2Set.isEmpty()){
-                        break;
-                    }
-
-                    while(!stair2Queue.isEmpty() && timeSol2>= stair2Queue.peek().stairTime){
-                       // System.out.println("조건만족");
-                        if (stair2Set.size() < 3) {
-                            stair2Set.add(stair2Queue.poll());
-                            //System.out.println("set 사이즈:"+ stair2Set.size());
-                        }else{
-                            //System.out.println("가득 참");
-                            break;
-                        }
-                        //System.out.println("jo22");
-
-                    }
-
-
-                }
-
-
-                //System.out.println("2에서 탈출시간" + timeSol2);
-
-                solTime = Math.min(solTime, Math.max(timeSol1, timeSol2));
-
             }
 
-            sb.append(solTime).append('\n');
-
-
+            answer = Integer.MAX_VALUE;
+            dfs(0, new int[personCount]);
+            sb.append("#").append(testCase).append(" ").append(answer).append("\n");
         }
 
         System.out.println(sb);
-
     }
 
-    public static int getDistance(int x, int y , int x1 , int y1) {
-        return Math.abs(x-x1)+Math.abs(y-y1);
+    static void dfs(int depth, int[] selected) {
+        if (depth == personCount) {
+            answer = Math.min(answer, simulate(selected));
+            return;
+        }
+
+        selected[depth] = 0;
+        dfs(depth + 1, selected);
+
+        selected[depth] = 1;
+        dfs(depth + 1, selected);
+    }
+
+    static int simulate(int[] selected) {
+        List<Integer>[] arrivalList = new ArrayList[2];
+        for (int i = 0; i < 2; i++) arrivalList[i] = new ArrayList<>();
+
+        for (int i = 0; i < personCount; i++) {
+            int stairChoice = selected[i];
+            arrivalList[stairChoice].add(people.get(i).arrival[stairChoice]);
+        }
+
+        int[] maxEnd = new int[2];
+
+        for (int s = 0; s < 2; s++) {
+            List<Integer> arrivals = arrivalList[s];
+            Collections.sort(arrivals);
+            PriorityQueue<Integer> pq = new PriorityQueue<>(); // 끝나는 시간
+
+            for (int time : arrivals) {
+                while (!pq.isEmpty() && pq.peek() <= time) {
+                    pq.poll();
+                }
+
+                if (pq.size() < 3) {
+                    pq.offer(time + stairs[s].time);
+                    maxEnd[s] = Math.max(maxEnd[s], time + stairs[s].time);
+                } else {
+                    int earliest = pq.poll();
+                    pq.offer(earliest + stairs[s].time);
+                    maxEnd[s] = Math.max(maxEnd[s], earliest + stairs[s].time);
+                }
+            }
+        }
+
+        return Math.max(maxEnd[0], maxEnd[1]);
     }
 }
