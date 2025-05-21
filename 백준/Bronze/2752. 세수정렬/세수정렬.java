8e81bace6ae36b6e62833d862e9fa23367b517c4
daeyeol-yang <97139430+daeyeol-yang@users.noreply.github.com>
2025-05-21 23:46:11 +0900

    [Bronze IV] Title: 세수정렬, Time: 188 ms, Memory: 18388 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[3];
+
+       
+        for (int i = 0; i < 3; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        
+        Arrays.sort(numbers);
+
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
